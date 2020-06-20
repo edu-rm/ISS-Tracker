55968a2121c70f79611dec7a89f43302397b7e0f
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-19 23:22:36 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+// import { Link } from 'react-router-dom';
 import './styles.css';
+import ISS from '../../assets/iss.png';
 
 function Header() {
-  return <h1>header</h1>;
+
+  return (
+    <div className="container-header">
+      <div className='content'>
+        <ul>
+          <div className="logo">
+            <img src={ISS} alt="iss logo"/>
+            <h2>ISS</h2>
+          </div>
+          <li className='active'>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='/sobre'>Sobre</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
