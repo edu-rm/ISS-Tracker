7c927727e31afd8cb3a52550bbeb9e9dac924015
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-20 10:56:27 -0300

    new style

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -52,8 +52,11 @@ function Inicio() {
       <div className="map">
         <Map center={[0,0]} zoom={2} >
           <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            // attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url='https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png'
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
           />
           <Marker position={iss}/>
         </Map>
