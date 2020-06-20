87225a7b52b524ce2b22a29c5cb9c02f2c5e7e1a
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-19 23:24:30 -0300

    map and card created

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
 function Inicio() {
-  return <h1>Inicio</h1>;
+  const [intial, setInitial] = useState([0,0]);
+  const [iss, setIss] = useState([51.79149569528,132.52919955419]);
+
+  return (
+    <div className="container-inicio">
+      <div className="map">
+        <Map center={intial} zoom={2} >
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={iss}/>
+        </Map>
+      </div>
+      <div className="info">
+        <h3>Information</h3>
+        <div className="info-content">
+          <p>Lat: 8543985493</p>
+          <p>Long: 8543985493</p>
+          <p>Lat: 8543985493</p>
+          <p>Long: 8543985493</p>
+          <p>Lat: 8543985493</p>
+          <p>Long: 8543985493</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Inicio;
